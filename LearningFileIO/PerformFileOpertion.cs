3ffabebf2670939b38e2daf4941eef4b54a86daf
ka1peshc <kalpesh111997@gmail.com>
2021-10-06 20:26:55 +0530

    Add:PerformFileOperation class to perform file operation

---
+++ LearningFileIO/PerformFileOpertion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace LearningFileIO
+{
+    class PerformFileOpertion
+    {
+        public const string FILEPATH = @"D:\Bridgelabz lecture\Dot net\Day 27 C# File handling\TestingTextFile.txt";
+
+        public static void FileExist()
+        {
+            if (File.Exists(FILEPATH))
+                Console.WriteLine("It exists");
+            else
+                Console.WriteLine("Not present");
+        }
+    }
+}
---
+++ LearningFileIO/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace LearningFileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            PerformFileOpertion.FileExist();
+        }
+    }
+}
